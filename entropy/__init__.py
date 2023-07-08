87fa93094753bf383a046d022c9807c21e5c572f
u8slvn <u8slvn@gmail.com>
2023-07-08 11:18:34 +0200

    refactor: fps component

--- entropy/__init__.py
+++ entropy/__init__.py
@@ -9,7 +9,6 @@ pygame.init()
 class Game:
     def __init__(self, title: str) -> None:
         self.title = title
-        self.show_fps = False
         self.running = False
         self.screen_size = (800, 600)
         self.fps = FPS(60.0)
@@ -17,28 +16,21 @@ class Game:
         pygame.display.set_caption(title=self.title)
         self.screen = pygame.display.set_mode(self.screen_size)
 
-    def display_fps(self):
-        if self.show_fps is True:
-            self.fps.render(self.screen)
-
-    def toggle_show_fps(self, key):
-        if key == pygame.K_F5:
-            self.show_fps = not self.show_fps
-
     def get_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.KEYUP:
-                self.toggle_show_fps(event.key)
+                self.fps.toggle(key=event.key)
             elif event.type == pygame.KEYDOWN:
                 ...
 
     def update(self):
-        self.screen.fill((0, 0, 0))
+        self.fps.update()
 
     def render(self):
-        self.display_fps()
+        self.screen.fill((0, 0, 0))
+        self.fps.render(self.screen)
         pygame.display.update()
         self.fps.tick()
 
--- entropy/components/fps.py
+++ entropy/components/fps.py
@@ -1,23 +1,33 @@
 import pygame
 
+from entropy.colors import WHITE, BLACK
+
 
 class FPS:
 
     def __init__(self, value: float) -> None:
         self.value = value
-        self.clock = pygame.time.Clock()
         self.font = pygame.font.SysFont("Verdana", 12)
-        self.fmt = "{fps} FPS"
+        self.clock = pygame.time.Clock()
+        self.visible = False
+        self.format = "{fps} FPS"
+        self.text = self._build_text()
+
+    def _build_text(self) -> pygame.Surface:
+        fps = self.format.format(fps=round(self.clock.get_fps()))
+        return self.font.render(fps, True, WHITE, BLACK)
+
+    def toggle(self, key: int) -> None:
+        if key == pygame.K_F5:
+            self.visible = not self.visible
 
     def tick(self) -> None:
         self.clock.tick(self.value)
 
-    @property
-    def text(self):
-        return self.font.render(self.get_current_fps(), True, (255, 255, 255))
+    def update(self) -> None:
+        if self.visible:
+            self.text = self._build_text()
 
-    def get_current_fps(self) -> str:
-        return self.fmt.format(fps=round(self.clock.get_fps()))
-
-    def render(self, display):
-        display.blit(self.text, (200,150))
+    def render(self, display) -> None:
+        if self.visible:
+            display.blit(self.text, (200, 150))
